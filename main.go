d0ad2a490906cc19d44959301cae4cb2914c2ba9
shn <142196840+shn-ust@users.noreply.github.com>
2024-05-02 04:41:04 +0000

    Added adm package.

--- main.go
+++ main.go
@@ -13,16 +13,16 @@ import (
 func main() {
 	const snapLen = 262144
 
-	networkInterface := "eth0"
+	networkInterface := "lo"
 	handle, err := pcap.OpenLive(networkInterface, snapLen, true, pcap.BlockForever)
 
 	if err != nil {
 		panic(err)
 	}
 
-	// if err := handle.SetBPFFilter("port 5000 or port 5001"); err != nil {
-	// 	panic(err)
-	// }
+	if err := handle.SetBPFFilter("port 5000 or port 5001"); err != nil {
+		panic(err)
+	}
 
 	defer handle.Close()
 
