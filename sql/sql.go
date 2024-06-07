5c49b649dbd711bd13a3ad706b13eb4a9a1543c8
shn <142196840+shn-ust@users.noreply.github.com>
2024-06-07 09:07:05 +0000

    Added batch  insert.]

--- sql/sql.go
+++ sql/sql.go
@@ -18,6 +18,24 @@ type PacketDetail struct {
 	DestPort  uint16
 }
 
+// convertParsedPacket is used to create an array of "PacketDetail" 
+// from an array of "ParsedPacket"
+func convertParsedPacket(packets []*parse.ParsedPacket) []PacketDetail {
+	var details []PacketDetail
+
+	for _, packet := range packets {
+		details = append(details, PacketDetail{
+			Timestamp: packet.TimeStamp,
+			SrcIP:     packet.SrcIP.String(),
+			SrcPort:   uint16(packet.SrcPort),
+			DestIP:    packet.DestIP.String(),
+			DestPort:  uint16(packet.DestPort),
+		})
+	}
+
+	return details
+}
+
 func InsertPacket(packet *parse.ParsedPacket, db *gorm.DB, mutex *sync.Mutex) bool {
 	mutex.Lock()
 	defer mutex.Unlock()
@@ -33,4 +51,20 @@ func InsertPacket(packet *parse.ParsedPacket, db *gorm.DB, mutex *sync.Mutex) bo
 	})
 
 	return res.Error == nil
+}
+
+// InsertPacketInBatch inserts a slice of packets
+func InsertPacketInBatch(db *gorm.DB, mutex *sync.Mutex, packets []*parse.ParsedPacket) error {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	db.AutoMigrate(&PacketDetail{})
+
+	return db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(convertParsedPacket(packets)).Error; err != nil {
+			return err
+		}
+
+		return nil
+	})
 }
--- sql/sql_test.go
+++ sql/sql_test.go
@@ -25,7 +25,7 @@ func TestInsertPacket(t *testing.T) {
 	})
 
 	if err != nil {
-		panic("Failed to connect to database")
+		t.Error("Failed to connect to database")
 	}
 
 	parsedPacket := parse.ParsePacket(packet)
@@ -33,7 +33,41 @@ func TestInsertPacket(t *testing.T) {
 	var mu sync.Mutex
 
 	if ok := InsertPacket(parsedPacket, db, &mu); !ok {
-		t.Errorf("Failed to insert packet data to table!")
+		t.Error("Failed to insert packet data to table!")
+	}
+}
+
+func TestInsertPacketInBatch(t *testing.T) {
+	var (
+		sourceIP   = net.ParseIP("127.0.0.1")
+		sourcePort = uint16(8080)
+		destIP     = net.ParseIP("127.0.0.1")
+		destPort   = uint16(5432)
+
+		sourceIP2   = net.ParseIP("127.0.0.2")
+		sourcePort2 = uint16(8080)
+		destIP2     = net.ParseIP("127.0.0.2")
+		destPort2   = uint16(5432)
+	)
+	packet := utils.CreatePacket(sourceIP, destIP, sourcePort, destPort)
+	packet2 := utils.CreatePacket(sourceIP2, destIP2, sourcePort2, destPort2)
+
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{
+		SkipDefaultTransaction: true,
+	})
+
+	if err != nil {
+		t.Error("Failed to connect to database")
+	}
+
+	parsedPacket := parse.ParsePacket(packet)
+	parsedPacket2 := parse.ParsePacket(packet2)
+
+	var mu sync.Mutex
+
+	packets := []*parse.ParsedPacket{parsedPacket, parsedPacket2}
+	if err := InsertPacketInBatch(db, &mu, packets); err != nil {
+		t.Errorf("Error inserting data in batches: %v", err)
 	}
 
 }
